3fe75e5c9c13249020ea2237c50d5a3ceff246cf
annaswitaj <annaswitaj.sw@gmail.com>
2021-01-27 16:49:09 +0100

    serializers lesson2

--- firma/flota/models.py
+++ firma/flota/models.py
@@ -7,3 +7,14 @@ class Osoba(models.Model):
     nazwisko = models.CharField(max_length=60, null=False)
     data_urodzenia = models.DateField()
     zarobki = models.DecimalField(max_digits=10, decimal_places=2, default=4000)
+
+    def __str__(self):
+        return self.imie + ' ' + self.nazwisko + ' ' + str(self.data_urodzenia)
+
+
+class Samochod(models.Model):
+    marka = models.CharField(max_length=60, null=False, default='Fiat')
+    model = models.CharField(max_length=60, null=False)
+    pojemnosc = models.IntegerField()
+    rok_produkcji = models.DateField()
+    wlasciciel = models.ForeignKey(Osoba, on_delete=models.SET_NULL, null=True)
